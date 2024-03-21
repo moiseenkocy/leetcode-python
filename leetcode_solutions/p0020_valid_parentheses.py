90471f7d048875c717d063eb0e4b68b7186cad0b
Andrei Moiseenko <moiseenko.cy@gmail.com>
2024-03-21 22:42:16 +0200

    Add implementation for #0020 Valid Parentheses.

--- leetcode_solutions/p0020_valid_parentheses.py
+++ leetcode_solutions/p0020_valid_parentheses.py
@@ -8,3 +8,16 @@ class Solution:
 
     def isValid(self, s: str) -> bool:
         """Solve problem #20 Valid Parentheses."""
+        stack = []
+
+        for c in s:
+            if c in "[({":
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                pair = stack.pop()
+                if c != {"{": "}", "[": "]", "(": ")"}.get(pair):
+                    return False
+
+        return not stack
