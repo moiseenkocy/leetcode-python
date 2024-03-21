1f3d5a104e39493ccc262499e1b7b38cdc7f6341
Andrei Moiseenko <moiseenko.cy@gmail.com>
2024-03-21 22:14:08 +0200

    Add tests for #0014 Longest Common Prefix.

---
+++ leetcode_solutions/p0014_longest_common_prefix.py
@@ -0,0 +1,10 @@
+"""Solution for problem #14 Longest Common Prefix.
+
+https://leetcode.com/problems/longest-common-prefix/
+"""
+
+class Solution:
+    """Container class for storing solution."""
+
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        """Solve problem #14 Longest Common Prefix."""
---
+++ tests/test_p0014_longest_common_prefix.py
@@ -0,0 +1,21 @@
+"""Tests for problem #14 Longest Common Prefix."""
+
+import pytest
+
+from leetcode_solutions.p0014_longest_common_prefix import Solution
+
+
+@pytest.mark.parametrize(
+    ("strs", "expected_result"),
+    [
+        (["flower","flow","flight"], "fl"),
+        (["dog","racecar","car"], ""),
+    ],
+)
+def test_longestCommonPrefix(strs: list[str], expected_result: str) -> None:
+    """Test longestCommonPrefix solution."""
+    solver = Solution()
+
+    result = solver.longestCommonPrefix(strs)
+
+    assert result == expected_result
