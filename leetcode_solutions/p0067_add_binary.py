57513df561fa8102f3a41b8f5d003acc7a9daa98
Andrei Moiseenko <moiseenko.cy@gmail.com>
2024-03-24 17:54:09 +0200

    Add solution for #67 Add Binary.

--- leetcode_solutions/p0067_add_binary.py
+++ leetcode_solutions/p0067_add_binary.py
@@ -9,3 +9,21 @@ class Solution:
 
     def addBinary(self, a: str, b: str) -> str:
         """Solve problem #67 Add Binary."""
+        len_a = len(a)
+        len_b = len(b)
+        max_len = max(len_a, len_b)
+
+        carry = 0
+        result = ""
+
+        for x, y in zip(
+            a[::-1] + "0" * (max_len - len_a),
+            b[::-1] + "0" * (max_len - len_b),
+            strict=False,
+        ):
+            carry, digit = divmod(int(x) + int(y) + carry, 2)
+            result += str(digit)
+        if carry:
+            result += "1"
+
+        return result[::-1]
