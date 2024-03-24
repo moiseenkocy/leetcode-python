926317162ca553e5549147a12b37fa9b8185ccf6
Andrei Moiseenko <moiseenko.cy@gmail.com>
2024-03-24 20:39:47 +0200

    Add solution for #69 Sqrt(x).

--- leetcode_solutions/p0069_sqrt_x.py
+++ leetcode_solutions/p0069_sqrt_x.py
@@ -9,3 +9,16 @@ class Solution:
 
     def mySqrt(self, x: int) -> int:
         """Solve problem #69 Sqrt(x)."""
+        left = 0
+        right = 65536
+
+        while right - left > 1:
+            middle = left + (right - left) // 2
+            if middle * middle == x:
+                return middle
+            if middle * middle > x:
+                right = middle
+            else:
+                left = middle
+
+        return left
